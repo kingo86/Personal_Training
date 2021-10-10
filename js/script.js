3819b5adce36f01fd36be2b066cf5b5c2dbd1f9f
Cartrell KIDD <cartrellk56@gmail.com>
2021-10-10 13:26:04 -0400

    Calculate Bmi

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
-fetch("https://fitness-calculator.p.rapidapi.com/bmi", {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
-    "x-rapidapi-key": "043557ced7mshe054f32b40fd78fp165e75jsn9cbd7995960c"
-  }
+var calcBmiButton = document.querySelector('#calc-button')
+var weight = document.querySelector('#weight')
+var height = document.querySelector('#height')
+var age = document.querySelector('#age')
+calcBmiButton.onclick = function calculateBmi() {
+fetch('https://fitness-calculator.p.rapidapi.com/bmi?age='+age.value+'&weight='+weight.value+'&height='+height.value+'', {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "043557ced7mshe054f32b40fd78fp165e75jsn9cbd7995960c"
+    }
 })
 .then(response => {
-  console.log(response);
+    console.log(response);
 })
 .catch(err => {
-  console.error(err);
+    console.error(err);
 });
+};
+// Add localStorage through persisting the input of the user
 function calculateBmi() {
-  var weight = document.bmiForm.weight.value
-  var height = document.bmiForm.height.value
-  if(weight > 0 && height > 0){ 
-  var finalBmi = weight/(height/100*height/100)
-  document.bmiForm.bmi.value = finalBmi
-  if(finalBmi < 18.5){
-  document.bmiForm.meaning.value = "John Doe"
-  }
-  if(finalBmi > 18.5 && finalBmi < 25){
-  document.bmiForm.meaning.value = "Nora Doe"
-  }
-  if(finalBmi > 25){
-  document.bmiForm.meaning.value = "Eric Doe"
-  }
-  }
-  else{
-  alert("Please Fill in everything correctly to get assigned to a Personal Trainer")
-  }
-  }
+    if(weight > 0 && height > 0){
+    var finalBmi = weight/(height/100*height/100)
+    document.bmiForm.bmi.value = finalBmi
+    if(finalBmi < 18.5){
+    document.bmiForm.meaning.value = "John Doe"
+    }
+    if(finalBmi > 18.5 && finalBmi < 25){
+    document.bmiForm.meaning.value = "Nora Doe"
+    }
+    if(finalBmi > 25){
+    document.bmiForm.meaning.value = "Eric Doe"
+    }
+    }
+    else{
+    alert("Please Fill in everything correctly to get assigned to a Personal Trainer")
+    }
+    }
